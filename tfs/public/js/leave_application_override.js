45f00d0feeab3072b1fa5902c80f3e0fb82102ee
ArunaDevi <arunadevi.m@techfinite.com>
2024-02-20 19:47:53 +0530

    modified changes

--- tfs/public/js/leave_application_override.js
+++ tfs/public/js/leave_application_override.js
@@ -1,6 +1,8 @@
 
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
+
+
 frappe.ui.form.on("Leave Application", {
     setup: function(frm) {
         frm.set_query("leave_approver", function() {
