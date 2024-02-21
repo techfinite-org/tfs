51cac142450ee6a535b045ef3ae80eadeab6ed61
Alwin.Johnson <alwin.johnson@techfinite.com>
2024-02-21 14:31:14 +0530

    Over Time Added In Compensatory Leave Request

--- tfs/compensatory_leave_request_overrides.py
+++ tfs/compensatory_leave_request_overrides.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import add_days, cint, date_diff, format_date, getdate
+from frappe.utils import add_days, cint, date_diff, format_date, getdate, time_diff_in_seconds
 
 from hrms.hr.utils import (
 	create_additional_leave_ledger_entry,
@@ -54,8 +54,10 @@ class CustomCompensatoryLeaveRequest(Document):
 					"You were only present for Half Day on {}. Cannot apply for a full day compensatory leave"
 				).format(", ".join([frappe.bold(format_date(half_day)) for half_day in half_days]))
 			)
-
-		if len(attendance_records) < date_diff(self.work_end_date, self.work_from_date) + 1:
+		leave_type_doc = frappe.get_doc('Leave Type', self.leave_type) 
+		if leave_type_doc and leave_type_doc.custom_hourly == 1:
+			return
+		elif len(attendance_records) < date_diff(self.work_end_date, self.work_from_date) + 1:
 			frappe.throw(_("You are not present all day(s) between compensatory leave request days"))
 
 	def validate_holidays(self):
@@ -72,12 +74,20 @@ class CustomCompensatoryLeaveRequest(Document):
    
 	def on_submit(self):
 		company = frappe.db.get_value("Employee", self.employee, "company")
-		date_difference = date_diff(self.work_end_date, self.work_from_date) + 1
+		leave_type_doc = frappe.get_doc('Leave Type', self.leave_type) 
+		if leave_type_doc and leave_type_doc.custom_hourly == 1:
+			date_time_difference = time_diff_in_seconds(self.custom_work_end_date_time, self.custom_work_from_date_time) / 60
+			date_difference = date_time_difference
+		else :
+			date_difference = date_diff(self.work_end_date, self.work_from_date) + 1
+		print(date_time_difference)
 		if self.half_day:
 			date_difference -= 0.5
 		leave_period = get_leave_period(self.work_from_date, self.work_end_date, company)
+		print(leave_period)
 		if leave_period:
 			leave_allocation = self.get_existing_allocation_for_period(leave_period)
+			print("--------------------------------leave_allocation------------------------",leave_allocation)
 			if leave_allocation:
 				leave_allocation.new_leaves_allocated += date_difference
 				leave_allocation.validate()
@@ -88,7 +98,6 @@ class CustomCompensatoryLeaveRequest(Document):
 				create_additional_leave_ledger_entry(
 					leave_allocation, date_difference, add_days(self.work_end_date, 1)
 				)
-
 			else:
 				leave_allocation = self.create_leave_allocation(leave_period, date_difference)
 			self.db_set("leave_allocation", leave_allocation.name)
@@ -162,3 +171,19 @@ class CustomCompensatoryLeaveRequest(Document):
 		allocation.insert(ignore_permissions=True)
 		allocation.submit()
 		return allocation
+
+@frappe.whitelist()
+def hide_unhide_date_time_field(leave_type):
+	leave_type_doc = None
+
+	if leave_type:
+		leave_type_doc = frappe.get_doc('Leave Type', leave_type)
+
+	# Check if leave_type_doc is not None and has 'custom_hourly' attribute
+	if leave_type_doc and leave_type_doc.custom_hourly == 1:
+		# print("----------------------",leave_type_doc.custom_hourly)
+		return leave_type_doc.custom_hourly
+
+	# Return 'hide' if leave_type is provided but another variable is not found
+	if leave_type:
+		return 'hide'
