3ffb75af60fd4f28c7e10c49dc68fa60b10fb7af
Santhosh20210024 <svallatharasu@techfinite.com>
2024-03-07 16:34:25 +0530

    changes on control panel configuration

--- tfs/tfs/doctype/control_panel/control_panel.js
+++ tfs/tfs/doctype/control_panel/control_panel.js
@@ -1,186 +1,146 @@
-// Copyright (c) 2023, Techfinite Systems and contributors
-// For license information, please see license.txt
-
-
 frappe.ui.form.on('Control Panel', {
-	process_debtors: function(frm){
+	process_debtors: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.run_transform.run_transform_process",
-			args:{
-				type:"debtors"
+			method: "agarwals.utils.run_transform.run_transform_process",
+			args: {
+				type: "debtors"
 			},
-			callback:function(r){
-				if(r.message != "Success"){
+			callback: function (r) {
+				if (r.message != "Success") {
 					frappe.throw(r.message)
 				}
-				else{
+				else {
 					frappe.msgprint("Debtors Reports are loaded")
 				}
 			}
 		})
 	},
-
-	map_tpa_in_bill: function(frm) {
-            frappe.call({
-					method: "agarwals.utils.tpa_and_region_matcher.match_tpa",
-					callback: function(r){
-						var return_msg = r.message
-						frappe.msgprint(return_msg)
-					}
-			})
-        },
-	map_region_in_bill:function(frm){
-            frappe.call({
-					method: "agarwals.utils.tpa_and_region_matcher.map_region",
-					callback: function(r){
-						var return_msg = r.message
-						console.log(return_msg)
-					}
-			})
-        },
-	map_branch_type_in_bill:function(frm){
-            frappe.call({
-					method: "agarwals.utils.tpa_and_region_matcher.map_branch_type",
-					callback: function(r){
-						var return_msg = r.message
-						console.log(return_msg)
-					}
-			})
-        },
-
-	create_sales_invoice:function(frm){
-            frappe.call({
-					method: "agarwals.utils.sales_invoice_creator.create_sales_invoice",
-			});
-        },
-
-    process_payment_using_payment_entry:function(frm){
-            frappe.call({
-					method: "agarwals.utils.run_transform.run_payment_entry",
-			});
-        },
-
-	process_claimbook: function(frm){
+	process_claimbook: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.run_transform.run_transform_process",
-			args:{
-				type:"claimbook"
+			method: "agarwals.utils.run_transform.run_transform_process",
+			args: {
+				type: "claimbook"
 			},
-			callback:function(r){
-				if(r.message != "Success"){
+			callback: function (r) {
+				if (r.message != "Success") {
 					frappe.throw(r.message)
 				}
-				else{
+				else {
 					frappe.msgprint("ClaimBook are loaded")
 				}
 			}
 		})
 	},
-	
-	process_settlement_stagging: function(frm){
+	process_settlement_staging: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.settlement_advice.advice_transform",
-			args:{
+			method: "agarwals.utils.settlement_advice.advice_transform",
+			args: {
 			},
-			callback:function(r){
-				if(r.message != "Success"){
+			callback: function (r) {
+				if (r.message != "Success") {
 					frappe.throw(r.message)
 				}
-				else{
+				else {
 					frappe.msgprint("Settlement Advice are loaded")
 				}
 			}
 		})
 	},
-
-	process_transaction_stagging: function(frm){
+	process_transaction_staging: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.run_transform.run_transform_process",
-			args:{
-				type:"transaction"
+			method: "agarwals.utils.run_transform.run_transform_process",
+			args: {
+				type: "transaction"
 			},
-			callback:function(r){
-				if(r.message != "Success"){
+			callback: function (r) {
+				if (r.message != "Success") {
 					frappe.throw(r.message)
 				}
-				else{
+				else {
 					frappe.msgprint("Bank transaction are loaded")
 				}
 			}
 		})
 	},
-
-	process_payment_entry: function(frm){
+	create_sales_invoice: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.payment_entry_job.create_payment_entries",
-		})
+			method: "agarwals.utils.sales_invoice_creator.create_sales_invoice",
+		});
 	},
-
-
-	mapping_insurance: function(frm) {
+	mapping_insurance: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.insurance_mapping.tag_insurance_pattern",
-			args:{
-				doctype:"Bank Transaction Staging",
+			method: "agarwals.utils.insurance_mapping.tag_insurance_pattern",
+			args: {
+				doctype: "Bank Transaction Staging",
 			},
-			callback:function(r){
-				if(r.message == "Done"){
+			callback: function (r) {
+				if (r.message == "Done") {
 					frappe.msgprint({
 						title: __('Notification'),
 						indicator: 'green',
-						message: __('Payment Insurance Tagging process is done 😀')
+						message: __('Insurance Tagging Process : Done')
 					});
 				}
 			}
 		})
 	},
-	process_bank_transaction: function(frm) {
+	process_bank_transaction: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.bank_transaction_process.process",
-			args:{
+			method: "agarwals.utils.bank_transaction_process.process",
+			args: {
 				tag: "Insurance",
 			},
-			callback:function(r){
-				if(r.message == "Success"){
+			callback: function (r) {
+				if (r.message == "Success") {
 					frappe.msgprint({
 						title: __('Notification'),
 						indicator: 'green',
-						message: __('Bank Transaction Processing is done 😀')
+						message: __('Bank Transaction Process : Done')
 					});
 				}
 			}
 		})
 	},
-	update_matcher: function(frm){
-	    frappe.call({
-	        method:"agarwals.utils.matcher.update_matcher"
-	    })
-	},
-	process_settlement_advice: function(frm) {
+	generate_claim_key: function (frm) {
 		frappe.call({
-			method:"agarwals.utils.settlement_advice_staging_process.process",
-			args:{
+			method: "agarwals.utils.claim_key_mapper.map_claim_key"
+		})
+	},
+	process_payment_using_payment_entry: function (frm) {
+		frappe.call({
+			method: "agarwals.utils.run_transform.run_payment_entry",
+		});
+	},
+	process_payment_using_journal_entry: function (frm) {
+		frappe.call({
+			method: "",
+		})
+	},
+	process_settlement_advice: function (frm) {
+		frappe.call({
+			method: "agarwals.utils.settlement_advice_staging_process.process",
+			args: {
 			},
-			callback:function(r){
-				if(r.message == "Success"){
+			callback: function (r) {
+				if (r.message == "Success") {
 					frappe.msgprint({
 						title: __('Notification'),
 						indicator: 'green',
 						message: __('Settlement Advice Staging transfer done')
 					});
 				}
-				else{
+				else {
 					frappe.throw(r.message)
 				}
 			}
 		})
 	},
-	download_file: function(frm) {
+	download_file: function (frm) {
 		frappe.call({
-			method:"agarwals.website_downloader.downloader_job.enqueue_job",
-			args:{
-				"tpa_name":frm.doc.tpa,
-		        "hospital_branch":frm.doc.branch
+			method: "agarwals.website_downloader.downloader_job.enqueue_job",
+			args: {
+				"tpa_name": frm.doc.tpa,
+				"hospital_branch": frm.doc.branch
 			}
 		})
 	}
