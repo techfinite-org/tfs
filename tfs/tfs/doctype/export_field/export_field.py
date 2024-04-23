af17a8bf642ded00e669d26461f97cb42ef12d67
Alwin.Johnson <alwin.johnson@techfinite.com>
2024-04-23 10:32:15 +0530

    Removed Unwanted doctypes

--- tfs/tfs/doctype/export_field/export_field.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Techfinite Systems and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class ExportField(Document):
-	pass
