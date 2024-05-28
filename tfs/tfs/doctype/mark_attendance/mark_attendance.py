4baf51cee665a63be2fa8125fe8c0a0043d20790
Alwin.Johnson <alwin.johnson@techfinite.com>
2024-05-28 17:03:23 +0530

    Removed Prints

--- tfs/tfs/doctype/mark_attendance/mark_attendance.py
+++ tfs/tfs/doctype/mark_attendance/mark_attendance.py
@@ -11,49 +11,27 @@ class MarkAttendance(Document):
 
 @frappe.whitelist()
 def get_all_shift():
-    # run_daily_attendance()
-    # Use frappe.get_all to retrieve a list of Shift Type documents
     shifts = frappe.get_all('Shift Type', filters={}, pluck='name')  # Add the fields you need
-    # shifts_with_double_quotes = '[' + ', '.join(f'"{shift}"' for shift in shifts) + ']'
-    # print("--------------------------shifts_with_double_quotes-------------------------",shifts_with_double_quotes)
-    # Do something with the list of shifts
-    # for shift in shifts:
-    #     print(shift.name)  # Example: Access the 'name' field of each Shift Type document
-    print("--------------------------shifts----------------------",shifts)
     return shifts  # Return the list if needed
-
-
 
 @frappe.whitelist()
 def update_shift_type_dates(process_attendance_after=None, last_sync_of_checkin=None):
-
-    # Check if both arguments are provided
-    if not process_attendance_after or not last_sync_of_checkin:       
+    if not process_attendance_after or not last_sync_of_checkin:
         frappe.msgprint(_("Please provide valid values for Process Attendance After and Last Sync of Checkin"))
         return
-    # Print the values for debugging (optional)
-    print("process_attendance_after:", process_attendance_after)
-    print("last_sync_of_checkin:", last_sync_of_checkin)
 
-    # Get all shifts and parse into a list of dictionaries
     shifts = frappe.get_all("Shift Type", filters={}, fields=["name"])
 
-    # Iterate through each shift and update Shift Type
     for shift in shifts:
         shift_type = shift.get('name')
-
         # Check if the Shift Type exists
         if not frappe.db.exists("Shift Type", shift_type):
             frappe.throw(_("Shift Type {0} does not exist").format(shift_type))
-
         # Fetch the Shift Type document
         shift_type_doc = frappe.get_doc("Shift Type", shift_type)
-
         # Update the fields
         shift_type_doc.process_attendance_after = process_attendance_after
         shift_type_doc.last_sync_of_checkin = last_sync_of_checkin
-        print("------------------------process_attendance_after-------------------",process_attendance_after)
-        print("------------------------last_sync_of_checkin-------------------", last_sync_of_checkin)
         # Save the changes
         shift_type_doc.save()
         frappe.db.commit()
