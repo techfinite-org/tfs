ae46ec0903359426cc7d6118fe312dccc582dd88
Kaarthik97 <kaarthik.n@techfinite.com>
2024-06-20 20:37:55 +0530

    pdf merge to file upload

--- tfs/tfs/doctype/pdf_extract/pdf_extract.py
+++ tfs/tfs/doctype/pdf_extract/pdf_extract.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+import zipfile
 import os
 import pdfplumber
 import json
@@ -10,61 +11,108 @@ from datetime import datetime
 import re
 from agarwals.utils.error_handler import log_error
 from frappe.model.document import Document
+from agarwals.agarwals.doctype.file_upload.file_upload import Fileupload
 
-# class PdfExtract(Document):
+class PdfExtract(Document):
+    pass
 @frappe.whitelist()
 def pdfwithtext(parent_name = None):
-    customer_name = []
-    folder = file_list(parent_name)
+    folder = []
+    if parent_name == None:
+        pdf_extract_list = frappe.get_all("Pdf Extract",{"is_parsed": False},pluck = "name")
+        for pdf_extract in pdf_extract_list:
+            if folder == []:
+                folder = file_list(pdf_extract)
+                pdf_parsing(folder, pdf_extract)
+    else:
+        folder = file_list(parent_name)
+        pdf_parsing()
+
+
+def pdf_parsing(folder, doc):
+    customer_list = []
     if folder:
-        customer_list = frappe.get_all("Pdf Parser",{},["tpa_name","customer","mapping"])
-        for every_customer in customer_list:
-            customer_name.append(every_customer.tpa_name)
+        customers = frappe.get_all("Pdf Parser",{},["tpa_name","customer","mapping"])
+        data_frame = pd.DataFrame()
+        today = datetime.now().strftime("%Y-%m-%d")
+        for every_customer in customers:
+            customer_list.append(every_customer.tpa_name)
         for every_file in folder:
-            if every_file.file_type == None or every_file.file_type.lower() != "pdf":
+            if every_file.file_type == None or every_file.file_type.lower() not in ["pdf","zip"]:
+                log_error(f"{every_file.file_type} is not supported")
                 continue
-            text = None
+            
+            
+            #set path
             base_path = os.getcwd()
             site_path = frappe.get_site_path()[1:]
             full_path = base_path+site_path
+            file_name = every_file.file_url.split("/")[-1]
             pdf_file = full_path+str(every_file.file_url)
-            pdffileobj = open(pdf_file, 'rb')
-            pdfreader = pdfplumber.open(pdffileobj)
-            x = len(pdfreader.pages)
-            for i in range(x):
-                page_obj = pdfreader.pages[i]
-                if text != None:
-                    text += page_obj.extract_text()
-                else:
-                    text = page_obj.extract_text()
-            cleaned_words = new_line(text)
-            if "Denial" in cleaned_words:
-                continue
-
-            customer = get_tpa_name(customer_name, cleaned_words)
-            json_data = get_sa_values(customer, cleaned_words,"Pdf Parser")
-            settled_amount = json_data["settled_amount"][0]
-            if settled_amount:
-                data_frame = pd.DataFrame(json_data)
-                today = datetime.now()
-                formatted_date = today
-                data_frame.to_csv(f"{full_path}/public/files/pp-{customer}-{settled_amount}-{formatted_date}.csv", index=False)
+            
+            #open file
+            if every_file.file_type.lower() == "zip":
+                extract_folder = f"{full_path}/private/files/DrAgarwals/Extract"
+                is_folder_exist(extract_folder)
+                pdf_list = unzip_files(pdf_file, extract_folder)
+                for pdf in pdf_list:
+                    pdffileobj = open(f"{extract_folder}/{pdf}", 'rb')
+                    cleaned_words = read_pdf(pdffileobj)
+                    data_frame, customer = process_pdf(customer_list, cleaned_words, data_frame)
             else:
-                log_error("no_settled_amount")
-    else:
-        log_error("no_pdf_to_parse")
+                pdffileobj = open(pdf_file, 'rb')
+                cleaned_words = read_pdf(pdffileobj)
+                data_frame, customer = process_pdf(customer_list, cleaned_words, data_frame)
+            
+            customer_name = frappe.get_all("Pdf Parser",{"tpa_name":customer},pluck= "customer")[0]
+            is_folder_exist(f"{full_path}/private/files/DrAgarwals/Pdf Extract")
+            full_file_path = f"{full_path}/private/files/DrAgarwals/Pdf Extract/pp-{customer_name}.csv"
+            file_path = f"/private/files/DrAgarwals/Pdf Extract/pp-{customer_name}.csv"
+            
+            data_frame.to_csv(full_file_path, index=False)
+            file_url = create_file_doc(full_file_path, file_path)
+            create_fileupload(file_url,doc,customer_name, today)
+        
+        
+        
+def create_file_doc(full_file_path, file_path):
+    file_name = full_file_path.split('/')[-1]
+    file = frappe.new_doc('File')
+    file.set('file_name', file_name)
+    file.set('is_private', 1)
+    file.set('file_url',file_path)
+    file.save()
+    frappe.db.commit()
+    return file.file_url
+    
+                    
+def read_pdf(pdffileobj):
+            #read pdf
+        text = None
+        pdfreader = pdfplumber.open(pdffileobj)
+        x = len(pdfreader.pages)
+        for i in range(x):
+            page_obj = pdfreader.pages[i]
+            if text != None:
+                text += page_obj.extract_text()
+            else:
+                text = page_obj.extract_text()
+        cleaned_words = new_line(text)
+        return cleaned_words
+        
+        
+def process_pdf(customer_list, cleaned_words, data_frame):
+        customer = get_tpa_name(customer_list, cleaned_words)
+        json_data = get_sa_values(customer, cleaned_words,"Pdf Parser")
+        data_frame = process_dataframe(json_data, data_frame)
+        return data_frame, customer
+            
 
 def file_list(parent_name):
     if parent_name:
         folder = frappe.get_all("File", {"attached_to_name": parent_name}, ["file_url", "file_name", "file_type"])
-    else:
-        folder = frappe.get_all("File", filters={"folder": "Home/Attachments"}, fields=["file_url", "file_name", "file_type"])
     return folder
         
-        
-def pattern(word):
-    pattern = r'\b' + re.escape(word) + r'\s+(\w+)'
-    return pattern
 
 def match(word, position, cleaned_words):
     match = re.search(word,cleaned_words)
@@ -98,12 +146,11 @@ def new_line(text):
     return new_line
 
 
-def get_tpa_name(customer_name, text):
+def get_tpa_name(customer_list, text):
     try:
-        for every_customer_name in customer_name:
+        for every_customer_name in customer_list:
             if every_customer_name in text:
-                tpa_name = every_customer_name
-                customer = tpa_name
+                customer = every_customer_name
                 return customer
     except Exception as e:
         log_error(e)
@@ -115,6 +162,7 @@ def get_sa_values(customer, cleaned_words,doctype):
         tpa_doc = frappe.get_doc(doctype, {"tpa_name": customer}, ["mapping"])
         tpa_json = tpa_doc.mapping
         data = json.loads(tpa_json)
+        date_format = tpa_doc.date_format
         json_data = {}
         json_data["name"] = [customer]
         for key, values in data.items():
@@ -136,12 +184,14 @@ def get_sa_values(customer, cleaned_words,doctype):
                 deductions = match(values["search"], values["index"], cleaned_words)
                 deductions = remove_comma(deductions)
                 json_data["deduction"] = [int(deductions)]
+            elif key == "paid_date":
+                paid_date = match(values["search"], values["index"], cleaned_words)
+                paid_date = format_date(paid_date,date_format)
+                json_data["paid_date"] = [paid_date]
         return json_data
     except Exception as e:
 
         log_error(e)
-
-
 
 
 def remove_comma(value):
@@ -149,4 +199,58 @@ def remove_comma(value):
         output_value = value.replace(",", "")
     else:
         output_value = value
+        
+    if "." in output_value:
+        output_value = output_value.split(".")[0]
+    else:
+        output_value = value
     return output_value
+
+
+def unzip_files(fpath= None, fdir= None):
+    try:
+        if os.path.exists(fpath):
+            with zipfile.ZipFile(fpath) as _zip:
+                zip_list = _zip.namelist()
+                is_folder_exist(fdir)
+                _zip.extractall(fdir)
+                return zip_list
+        else:
+            frappe.throw("The corresponding zip file is not found on the specified location")
+            return False
+    except Exception as e:
+        log_error('File upload', str(e))
+        frappe.throw('Error while processing the zip files')
+        
+        
+def is_folder_exist(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+        
+def format_date(paid_date, date_format):
+    try:
+        formatted_date = datetime.strptime(paid_date,date_format)
+        return formatted_date
+    except Exception as e :
+        log_error(str(e))
+        
+        
+def process_dataframe(json_data, data_frame):
+        new_data = pd.DataFrame(json_data)
+        data_frame = data_frame._append(new_data, ignore_index = True)    
+        return data_frame
+    
+    
+def create_fileupload(file_url,file_list,customer_name, today):
+    file_upload_doc=frappe.new_doc("File upload")
+    file_upload_doc.document_type= "Settlement Advice"
+    file_upload_doc.payer_type= customer_name
+    file_upload_doc.upload= file_url
+    file_upload_doc.is_bot= 1
+    file_upload_doc.file_format= 'EXCEL'
+    file_upload_doc.save(ignore_permissions=True)
+    update_file = frappe.get_doc("Pdf Extract", file_list)
+    update_file.is_parsed = True
+    update_file.parsed_date = today
+    update_file.save()
+    frappe.db.commit()
