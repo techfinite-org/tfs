8176e4cf0dfa8657b2c3af1bc2a77ec4cfd5a65f
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-07-11 11:07:06 +0530

    Profiler

---
+++ tfs/profiler/stack.py
@@ -0,0 +1,29 @@
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+@singleton
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def __str__(self):
+        return str(self.stack)
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+
---
+++ tfs/profiler/timer.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import time
+from tfs.profiler.stack import Stack
+
+class Timer():
+    def __init__(self):
+        self.parent = None
+        self.child = []
+        self.name = None
+        self.start_time = None
+        self.end_time = None
+        self.ctx = None
+
+    def add(self, child):
+        self.child.append(child)
+
+    def start(self, name):
+        self.name = name
+        self.ctx = self.name
+        if not Stack().is_empty():
+            self.parent = Stack().peek()
+            self.parent.add(self)
+            if self.parent.ctx:
+                self.ctx = str(self.parent.ctx) + ">" + str(self.name)
+        self.start_time = round(time.time() * 1000)
+        Stack().push(self)
+        return self
+
+    def end(self):
+        if Stack().is_empty():
+            raise Exception("Empty Stack")
+        self.end_time = round(time.time() * 1000)
+        while not Stack().is_empty():
+            last = Stack().pop()
+            if last.name == self.name:
+                break
+
+    def print(self, file_name):
+        overall_time = self.end_time - self.start_time
+        cumulative_time = 0
+        for child in self.child:
+            cumulative_time += child.print(file_name)
+        self_time = overall_time - cumulative_time
+        with open(f"{file_name}.txt", "a") as file:
+            file.write(f"{self.ctx},{self.start_time},{self.end_time},{overall_time},{self_time}\n")
+        print(f"{self.ctx},{self.start_time},{self.end_time},{overall_time},{self_time}")
+        return overall_time
+
+
+
+
+
